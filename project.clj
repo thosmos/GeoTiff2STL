f42c3ac271c53ed80604407a2720dfc0293ff95c
Thomas Spellman <thos37@gmail.com>
2017-10-31 13:44:04 -0700

    tested the latest twelvemonkeys code for float32 read problem - no workie

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.geotools/gt-main "14.3"]
                  [org.geotools/gt-shapefile "14.3"]
-                 [com.twelvemonkeys.imageio/imageio-tiff "3.3.1"]
+                 [com.twelvemonkeys.imageio/imageio-tiff "3.4-SNAPSHOT"]
                  [com.heightmap/heightmap2stl "0.1.1"]
                  [org.clojure/tools.cli "0.3.5"]]
   ;:resource-paths ["lib/heightmap2stl.jar"]
--- src/hellogeotiff.clj
+++ src/hellogeotiff.clj
@@ -310,12 +310,15 @@
 
     (println "Calculating samples and min/max (z-lift: " z-lift ", mult: " multiplier ")")
     (let [keys (vec (for [w (range width) h (range height)]
-                  [w h]))
+                      [w h]))
           samples (vec (for [[w h] keys]
-                     (let [s (.getSample raster w h 0)]
-                       (if (> s 0)
-                         (+ z-lift (* multiplier (/ s 100.0)))
-                         0))))
+                         (let [s (.getSample raster w h 0)
+                               s2 (.getRGB image w h)
+                               s' (if (> s 0)
+                                    (+ z-lift (* multiplier (/ s 100.0)))
+                                    0)]
+                           (println s2 s s')
+                           s')))
           min (reduce min-fn samples)
           max (reduce max samples)]
       (println "min: " min "max: " max)
@@ -326,7 +329,7 @@
         (let [[w h] (get keys i)
               sample (get samples i)]
           (aset-float hmap (- (- width 1) w) h
-                     sample))))
+            sample))))
 
     ;(doseq [w (range width) h (range height)]
     ;  (let [s (.getSample raster w h 0)]
