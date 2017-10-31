2f41a3e52c41137bcd8a45321c3ff0e1aa5388d6
Thomas Spellman <thos37@gmail.com>
2017-10-31 10:13:40 -0700

    Code cleanup

--- project.clj
+++ project.clj
@@ -1,21 +1,12 @@
 (defproject geotiff2stl "1.0.0-SNAPSHOT"
-  :description "FIXME: write"
-  ;:repositories { ;"osgeo-geotools" "http://download.osgeo.org/webdav/geotools"
-  ;               ;"local" "file:maven_repository"
-  ;               }
+  :description "Convert GeoTIFF files to STL"
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 ;[org.geotools/gt-main "14.3"]
-                 ;[org.geotools/gt-shapefile "14.3"]
-                 ;[com.twelvemonkeys.imageio/imageio-tiff "3.3.2"]
-                 ;[com.heightmap/heightmap2stl "0.1.1"]
                  [org.clojure/tools.cli "0.3.5"]
                  [org.apache.commons/commons-lang3 "3.6"]
-                 ;[im.chit/lucid.core.java "1.3.13"]
                  [org.gdal/gdal "2.2.0"]]
   :jvm-opts ["-Djava.library.path=.:/usr/local/opt/gdal2/lib"]
   :java-source-paths ["java"]
   :source-paths ["src"]
   :main hellogeotiff; ^:skip-aot hellogeotiff
-  ;:target-path "target/%s"
   :profiles {:uberjar {:aot [hellogeotiff] }})
 
--- src/hellogeotiff.clj
+++ src/hellogeotiff.clj
@@ -1,11 +1,6 @@
 (ns hellogeotiff
   (:gen-class)
-  ;(:import [org.geotools.data CachingFeatureSource FeatureSource FileDataStore FileDataStoreFinder])
   (:import
-    ;[java.io File]
-    ;[java.awt.image BufferedImage]
-    ;[java.awt.image Raster]
-    ;[javax.imageio ImageIO ImageReader]
     [com.heightmap.stl StlObject ModelObject]
     [org.gdal.gdalconst gdalconstConstants]
     [org.gdal.gdal gdal Dataset Driver Band])
@@ -15,17 +10,8 @@
             [clojure.string :as string]))
 
 (defn reader-init []
-
   (try (gdal/AllRegister)
-       (catch Exception ex (println "Is GDAL installed and working?\n" (.getMessage ex))))
-
-  ;(doseq [suffix (ImageIO/getReaderFileSuffixes)]
-  ;  (println "suffix:" suffix))
-  ;
-  ;(let [readers (ImageIO/getImageReadersByFormatName "TIFF")]
-  ;  (while (. readers hasNext)
-  ;    (println "reader: " (. readers next))))
-  )
+       (catch Exception ex (println "Is GDAL installed and working?\n" (.getMessage ex)))))
 
 (defn ^String getDataType [v]
   (case v
@@ -43,15 +29,7 @@
     11 "CFloat64"
     12 "CFloat64"))
 
-(defn min-fn
-  ([x y]
-   (if (> y 0)
-     (min x y)
-     (if (> x 0)
-       x
-       Float/MAX_VALUE)))
-  ([x y & more]
-   (reduce min-fn (min-fn x y) more)))
+
 
 (def cli-options
   ;; An option with a required argument
@@ -108,6 +86,15 @@
   ;(System/exit status)
   )
 
+(defn min-fn
+  ([x y]
+   (if (> y 0)
+     (min x y)
+     (if (> x 0)
+       x
+       Float/MAX_VALUE)))
+  ([x y & more]
+   (reduce min-fn (min-fn x y) more)))
 
 (defn -main
   "The Main."
@@ -118,7 +105,7 @@
   (let [{:keys [filename options exit-message ok?]} (validate-args args)]
     (if exit-message
       (exit (if ok? 0 1) exit-message)
-      (let [name (.getName (io/as-file filename))
+      (let [^String name (.getName (io/as-file filename))
             ^float multiplier (:multiplier options)
             ^float z-lift (:z-lift options)
             ^Dataset ds (gdal/Open filename gdalconstConstants/GA_ReadOnly)
@@ -127,7 +114,7 @@
             y (.getRasterYSize ds)
             hmap (make-array Float/TYPE x y)
             ^Band band (.GetRasterBand ds 1)
-            minMax (double-array 2)
+            ^doubles minMax (double-array 2)
             _ (.ComputeRasterMinMax band minMax)
             sum-start {:min Float/MAX_VALUE :max Float/MIN_VALUE}
             sum-fn (fn [sum next]
@@ -148,91 +135,18 @@
         (let [sum (reduce sum-fn sum-start
                     (for [i (range x)
                           j (range y)]
-                      (let [v (float (+ z-lift (* (aget hmap i j) multiplier 0.01)))]
+                      (let [v (float (+ z-lift (* (aget hmap i j) multiplier 0.01)))
+                            v' (float (max 0.0 v))]
+                        ;; use the zeroed value for the build
+                        (aset hmap i j v')
                         (when (= (mod i 10) j 0)
                           (print ".") (flush))
-                        (aset hmap i j v)
                         v)))]
-          (println "Done parsing: Modified Min: " (:min sum) ", Max: " (:max sum)))
+          (println "\nDone parsing: Modified Min: " (:min sum) ", Max: " (:max sum)))
         (println "Saving STL")
         (let [stl (StlObject/fromHeightmap name y x hmap)
               name (if (.contains name ".")
                      (.substring name 0 (.lastIndexOf name "."))
                      name)]
           (set! (. stl -path) (str name ".stl"))
-          (.save stl StlObject/FILE_BINARY))
-        )))
-
-  ;(let [{:keys [action options exit-message ok?]} (validate-args args)]
-  ;  (if exit-message
-  ;    (exit (if ok? 0 1) exit-message)
-  ;    (let [^String filename action
-  ;          ;_ (println "OPTIONS " options)
-  ;          multiplier (:multiplier options)
-  ;          z-lift (:z-lift options)
-  ;          ^File file (File. filename)
-  ;          name (.getName file) ;
-  ;          ^BufferedImage image (try
-  ;                                 (ImageIO/read file)
-  ;                                 (catch Exception ex (exit 1 ex)))
-  ;          width (.getWidth image)
-  ;          _ (println "width" width)
-  ;          height (.getHeight image)
-  ;          _ (println "height" height)
-  ;          ^Raster raster (.getData image)
-  ;          hmap (make-array Float/TYPE width height)
-  ;          summary {:min Float/MAX_VALUE :max Float/MIN_VALUE}
-  ;          sum-fn (fn [sum next]
-  ;                   (let [[x y] next
-  ;                         v (.getSample raster x y 0)
-  ;                         v (+ z-lift (* multiplier (/ v 100.0)))
-  ;                         min (min-fn (:min sum) v)
-  ;                         max (max (:max sum) v)
-  ;                         ]
-  ;                     ;(aset-float hmap (- (- width 1) x) y v)
-  ;                     ;(when (= (mod x 10) 0)
-  ;                     ;  (println x y v))
-  ;                     {:min min :max max}))]
-  ;
-  ;      (println "Calculating samples and min/max (z-lift: " z-lift ", mult: " multiplier ")")
-  ;
-  ;      (println "RESULTS"
-  ;        (reduce sum-fn summary (for [w (range width) h (range height)] [w h])))
-  ;
-  ;
-  ;      ;
-  ;      ;(let [keys (vec (for [w (range width) h (range height)]
-  ;      ;                  [w h]))
-  ;      ;      samples (vec (for [[w h] keys]
-  ;      ;                     (let [s (.getSample raster w h 0)]
-  ;      ;                       (if (> s 0)
-  ;      ;                         (+ z-lift (* multiplier (/ s 100.0)))
-  ;      ;                         0))))
-  ;      ;      ;_ (println "samples " samples)
-  ;      ;      min (reduce min-fn samples)
-  ;      ;      max (reduce max samples)]
-  ;      ;  (println "min: " min "max: " max)
-  ;      ;  (when (> min 10)
-  ;      ;    (println "min is over 10, consider a lower z-lift value"))
-  ;      ;  (println "Converting to java array")
-  ;      ;  (doseq [i (range (count samples))]
-  ;      ;    (let [[w h] (get keys i)
-  ;      ;          sample (get samples i)]
-  ;      ;      (aset-float hmap (- (- width 1) w) h
-  ;      ;        sample))))
-  ;      ;
-  ;      ;(println "Saving STL")
-  ;      ;(let [stl (StlObject/fromHeightmap name height width hmap)
-  ;      ;      name (if (.contains name ".")
-  ;      ;             (.substring name 0 (.lastIndexOf name "."))
-  ;      ;             name)]
-  ;      ;  (set! (. stl -path) (str name ".stl"))
-  ;      ;  (.save stl StlObject/FILE_BINARY))
-  ;
-  ;      ;(println "count loop-recur" (time (count (points-loop width height hmap))))
-  ;      ;(println "count for-concat" (time (count (points-concat width height hmap))))
-  ;      ;(println "count for-array" (time (count (points-array width height hmap))))
-  ;
-  ;      )))
-
-  )
+          (.save stl StlObject/FILE_BINARY))))))
